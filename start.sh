2545aaf00d2eccafc42b05e56fa436029561ae15
Brxnin-FX <brunosantiago7002@gmail.com>
2025-01-03 10:22:29 -0300

    teste

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Aceitar o EULA automaticamente
-if [ "$MINECRAFT_EULA" = "true" ]; then
+if [ "$EULA" = "true" ]; then
   echo "eula=true" > /data/eula.txt
 else
   echo "Você precisa aceitar o EULA para iniciar o servidor."
@@ -10,11 +10,3 @@ fi
 
 # Iniciar o servidor Minecraft com Forge
 java -Xmx1024M -Xms1024M -jar /data/server.jar nogui &
-
-# Esperar o servidor iniciar completamente (pode precisar de alguns segundos)
-sleep 20
-
-
-
-# Manter o script em execução para não terminar
-wait
