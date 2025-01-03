358812ffea783177322b2a2d95658b586e1b5910
Brxnin-FX <brunosantiago7002@gmail.com>
2025-01-03 10:18:28 -0300

    test

--- start.sh
+++ start.sh
@@ -14,14 +14,7 @@ java -Xmx1024M -Xms1024M -jar /data/server.jar nogui &
 # Esperar o servidor iniciar completamente (pode precisar de alguns segundos)
 sleep 20
 
-# Adicionar um nome na whitelist automaticamente
-# Comando para adicionar "bruno" na whitelist
-echo "whitelist add Brxnin-FX" | nc -w 1 localhost 25565
 
-# Você pode adicionar outros comandos aqui, como:
-# echo "gamerule keepInventory true" | nc -w 1 localhost 25565
-# echo "gamerule doDaylightCycle false" | nc -w 1 localhost 25565
-# echo "gamerule doMobSpawning false" | nc -w 1 localhost 25565
 
 # Manter o script em execução para não terminar
 wait
